3ab1c96c862e1dde42984519ad2d4d7c88d698d2
Jon-Mikel <garrisonhgrave@yahoo.com>
2026-09-21 10:57:20 -0500

    SWAPI dem

--- APIS/SWAPIFrontEnd/script.js
+++ APIS/SWAPIFrontEnd/script.js
@@ -1,6 +1,8 @@
-
+let STARSHIP_ENDPOINT = "ship";
 const BASEURL = "https://swapi.dev/api"
 let route = "people"
+let container = document.getElementById("display_container")
+
 
 // let endpoint = BASEURL + "/" + route; // concatenation
 let endpoint = `${BASEURL}/${route}`; // template literal or string formatting
@@ -15,8 +17,32 @@ fetch(endpoint)
         }
     })
     .then(parsedData => {
-        console.log(parsedData.results[0].hair_color);
-        console.log(parsedData["results"][0]["hair_color"]);
-        console.log(parsedData["results"][0]["starships"][1]);
+        for (let i =0; i < parsedData["results"].length; i ++) {
+            console.log(parsedData.results[i].name);
+            console.log(parsedData.results[i].hair_color);
+            container.innerHTML += `
+                <div class="blue">
+                    <h1>${parsedData["results"][i]["name"]}</h1>
+                    <p>${parsedData["results"][i]["hair_color"]}</p>
+                </div>
+              
+                    `
+
+        }
+
+        // console.log(parsedData["results"][0]["hair_color"]);
+        // console.log(parsedData["results"][0]["starships"][1]);
+        // STARSHIP_ENDPOINT= parsedData["results"][0]["starships"][0];
+
+
+
+
+
     })
     .catch(err => console.error(err))
+// console.log(STARSHIP_ENDPOINT)
+//
+// fetch(STARSHIP_ENDPOINT)
+//     .then(newResponse => newResponse.json())
+//     .then(data => console.log("Ship name: ", data.name) )
+//     .catch(err => console.log(err))
