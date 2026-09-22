6e191ca36c02297e20d90ddd23603799cc631219
Jon-Mikel <garrisonhgrave@yahoo.com>
2026-09-22 11:41:41 -0500

    node demos

---
+++ Node/FirstNodeProject/index.js
@@ -0,0 +1,5 @@
+// how do we import
+// backend we have to require it!!
+const catMe = require("cat-me")
+
+console.log(catMe())
---
+++ Node/YesNoWordsAndMore/index.js
@@ -0,0 +1,24 @@
+import yesNoWords from "yes-no-words"
+
+let result = yesNoWords.yesRandom()
+console.log("YesNoWords - " + result)
+
+// //array of 10 yes words
+// // loop
+// let emptyArray = []
+// for(let i = 0; i < 10; i++){
+//     emptyArray[i] = yesNoWords.yesRandom()
+//     // emptyArray.push(yesNoWords.yesRandom())
+// }
+// console.log(emptyArray)
+// //  Array prototype options!!!
+// let monsterArray = Array(10).fill(Math.floor(Math.random()*20)+1)
+// console.log(monsterArray)
+
+
+// let str = "345dog"
+// let numResult =Number(str)
+
+// falsey = "", undefined, null, 0
+// truthy = " ", if it exists "bgadfhafsharhaehadhadfhj", 1
+// values
